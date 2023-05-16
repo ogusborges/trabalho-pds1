786518aee9ad89438b889551514948c036d18d0e
Gustavo Borges <ogusborges@gmail.com>
2023-05-15 22:15:48 -0300

    Adicionado arquivos de front-end/back-end

---
+++ back-end/src/test/kotlin/br/ufu/sisegresso/SistemaDeMonitoramentoDeEgressosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.ufu.sisegresso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaDeMonitoramentoDeEgressosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
