3edb444b59297e13c9d604a02887b8ce550cc08f
Gustavo Borges <ogusborges@gmail.com>
2023-06-12 22:43:20 -0300

    fix: merge

--- back-end/src/main/kotlin/br/ufu/sisegresso/controller/PessoaController.kt
+++ back-end/src/main/kotlin/br/ufu/sisegresso/controller/PessoaController.kt
@@ -1,21 +1,12 @@
 package br.ufu.sisegresso.controller
 
 import br.ufu.sisegresso.dtos.AtualizacaoPessoaDTO
-import br.ufu.sisegresso.dtos.RegistroPessoaDTO
 import br.ufu.sisegresso.service.IPessoaService
-import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-<<<<<<< HEAD
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-=======
-import org.springframework.web.bind.annotation.PatchMapping
->>>>>>> main
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
